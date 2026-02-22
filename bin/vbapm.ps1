5ca2c3fc9921cb788417c32c10146e875665a504
Martin Leduc <31558169+DecimalTurn@users.noreply.github.com>
2026-02-22 00:16:24 -0500

    fix: add fallback on system's node

--- bin/vbapm.ps1
+++ bin/vbapm.ps1
@@ -1,5 +1,10 @@
 #!/usr/bin/env pwsh
 $basedir=Split-Path $MyInvocation.MyCommand.Definition -Parent
 
-& "$basedir/../vendor/node.exe" --no-warnings "$basedir/../lib/vbapm.js" $args
+$vendorNode = Join-Path $basedir "../vendor/node.exe"
+if (Test-Path $vendorNode) {
+  & $vendorNode --no-warnings "$basedir/../lib/vbapm.js" $args
+} else {
+  & node --no-warnings "$basedir/../lib/vbapm.js" $args
+}
 exit $LASTEXITCODE
