9478dc1470582bd0a48c3c9c414d08f2ef583492
Martin Leduc <31558169+DecimalTurn@users.noreply.github.com>
2026-02-21 23:23:08 -0500

    feat: update test infrastructure for npm package module resolution

--- e2e.config.js
+++ e2e.config.js
@@ -2,8 +2,17 @@ module.exports = {
 	preset: "ts-jest",
 	testEnvironment: "node",
 	testMatch: ["**/tests/**/*.e2e.ts"],
+	testPathIgnorePatterns: ["/node_modules/", "/lib/"],
+	moduleNameMapper: {
+		"^vbapm$": "<rootDir>/src/index.ts"
+	},
 	snapshotFormat: {
 		escapeString: true,
 		printBasicPrototype: true
+	},
+	globals: {
+		"ts-jest": {
+			tsconfig: "tests/tsconfig.json"
+		}
 	}
 };
